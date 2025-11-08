e8585368aeacdd25abba96c2f3ed3791ef358aed
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-11-08 17:54:10 +0530

    Routing

--- src/app.js
+++ src/app.js
@@ -2,22 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send("User : Jay");
+});
+
 app.use("/test", (req, res) => {
   res.send("Test Route");
 });
 
-app.use("/userData",(req,res)=>(
-    res.send("User Data")
-))
+app.post("/newuser",(req,res)=>{
+  res.send("Hello")
+})
 
-app.use("/", (req, res) => {
-  res.send("home page");
-});
 
+// app.use("/userData",(req,res)=>(
+//     res.send("User Data")
+// ))
+
+// app.use("/", (req, res) => {
+//   res.send("home page");
+// });
 
 app.listen(3000, () => {
   console.log("App is listening on the port 3000");
 });
-
-
-       
