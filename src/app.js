8651d8b33f98771f1949b5ff5993fa53d6b049b1
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-11-09 13:23:48 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,41 +1,10 @@
 const express = require("express");
 const app = express();
 
-// app.use(
-//   "/user",
-//   (res, req, next) => {
-//     console.log("1st Response!!");
-//     next();
-//   },
-//   (req, res, next) => {
-//     console.log("2nd Response!!");
-//     next();
-//   },
-//   (req, res, next) => {
-//     console.log("3nd Response!!");
-//     next();
-//   },
-//   (req, res, next) => {
-//     console.log("4nd Response!!");
-//     next();
-//   },
-//   (req, res, next) => {
-//     res.send("5th Response!!");
-//   }
-// );
+const { authMiddleware } = require("./middlewares/auth.js");
 
 // auth middleware
-app.use("/admin", (req, res, next) => {
-  // use is for all the http requests (GET, PUT, POST,DELETE)
-  console.log("User Auth is Checked");
-  const token = "xyz";  // if the token is valid then it will move to the next route
-  const authorizedUser = token === "xyz";
-  if (!authorizedUser) {
-    res.status(401).send("Unauthorized User");
-  } else {
-    next();
-  }
-});
+app.use("/admin", authMiddleware);
 
 app.get("/user", (req, res) => {
   res.send("User Data");
