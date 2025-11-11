e7b60467079c2a19c5f3be3995511f98fbe1710d
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-11-11 15:40:01 +0530

    schema updated, validation added

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User Added");
   } catch (err) {
-    res.status(400).send("User Not Found");
+    res.status(400).send(err.message);
   }
 });
 
@@ -57,10 +57,12 @@ app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data); // or we can write (userId, data)
-    res.send("User Updated");
-  } catch (error) {
-    res.status(404).send("Something went wrong");
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true, // this is for the enbaling the validation
+    }); // or we can write (userId, data)
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(404).send("Update Failed" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,70 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/1553217327/vector/user-profile-icon-avatar-person-sign-profile-picture-portrait-symbol-easily-editable-line.jpg?s=170667a&w=0&k=20&c=xUuHLFaa94WIFdV-XBgxX9SSsaJJgGQhE1Tmevqrytg=",
+    },
+    about: {
+      type: String,
+      default: "Default About of User",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//email check
+userSchema.pre("save", async function (next) {
+  const existingUser = await mongoose.models.User.findOne({
+    emailId: this.emailId,
+  });
+
+  if (existingUser) {
+    const err = new Error("Email already exists");
+    err.status = 400;
+    return next(err);
+  }
+
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
