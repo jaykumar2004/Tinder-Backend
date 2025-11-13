4b0e3c79470990961faf7df552e29c62d35b65a7
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-11-13 14:53:37 +0530

    Backend

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const connectDb = require("./config/database.js");
 const User = require("./models/user.js");
 const bcrypt = require("bcrypt");
-const { validateSignupData } = require("./utils/validation.js");
+const {validateSignupData} = require("./utils/validation.js");
 const cookieParser = require("cookie-parser");
-
 const app = express();
 
 app.use(express.json());
@@ -12,138 +11,138 @@ app.use(cookieParser());
 
 //signup api
 app.post("/signup", async (req, res) => {
-  try {
-    //validation of data
-    validateSignupData(req);
+    try {
+        //validation of data
+        validateSignupData(req);
 
-    const { firstName, lastName, emailId, password } = req.body;
-    //bcrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
+        const {firstName, lastName, emailId, password} = req.body;
+        //bcrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
 
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
 
-    await user.save();
-    res.send("User Added");
-  } catch (err) {
-    res.status(400).send("Error : " + err.message);
-  }
+        await user.save();
+        res.send("User Added");
+    } catch (err) {
+        res.status(400).send("Error : " + err.message);
+    }
 });
 
 //login api
 app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId: emailId });
-    if (!user) {
-      throw new Error("Invalid Credentials");
-    }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (isPasswordValid) {
-      //creata a jwt token
+    try {
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId: emailId});
+        if (!user) {
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            //creata a jwt token
 
-      //add the token to cookie and send the response back to the user
-      res.cookie("token", "adkjgfyagdhjbvfghjasdfb");
-      res.send("Login Successfully");
-    } else {
-      throw new Error("Invalid Credentials");
+            //add the token to cookie and send the response back to the user
+            res.cookie("token", "adkjgfyagdhjbvfghjasdfb");
+            res.send("Login Successfully");
+        } else {
+            throw new Error("Invalid Credentials");
+        }
+    } catch (err) {
+        res.status(400).send("Error : " + err.message);
     }
-  } catch (err) {
-    res.status(400).send("Error : " + err.message);
-  }
 });
 
 //Get User by email
 app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-  try {
-    const users = await User.find({ emailId: userEmail });
-    if (users.length === 0) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(users);
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({emailId: userEmail});
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong");
     }
-  } catch (error) {
-    res.status(400).send("Something went wrong");
-  }
 });
 
 // Feed api - GET /feed - get all the users from the database
 app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(404).send("Something Went Wrong");
-  }
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(404).send("Something Went Wrong");
+    }
 });
 
 //delete api
 app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete(userId);
-    res.send("User Deleted");
-  } catch (error) {
-    res.status(404).send("Something Went Wrong");
-  }
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User Deleted");
+    } catch (error) {
+        res.status(404).send("Something Went Wrong");
+    }
 });
 
 //update user api
 app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
+    const userId = req.params?.userId;
+    const data = req.body;
 
-  try {
-    const ALLOWED_UPDATED = [
-      "userId",
-      "photoUrl",
-      "about",
-      "gender",
-      "age",
-      "skills",
-    ];
+    try {
+        const ALLOWED_UPDATED = [
+            "userId",
+            "photoUrl",
+            "about",
+            "gender",
+            "age",
+            "skills",
+        ];
 
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATED.includes(k)
-    );
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATED.includes(k)
+        );
 
-    if (!isUpdateAllowed) {
-      throw new Error(" : Update Not Allowed");
+        if (!isUpdateAllowed) {
+            throw new Error(" : Update Not Allowed");
+        }
+
+        if (data?.skills?.length > 10) {
+            throw new Error(" : Skills cannot be more than 10");
+        }
+
+        await User.findByIdAndUpdate({_id: userId}, data, {
+            runValidators: true, // this is for the enbaling the validation
+        }); // or we can write (userId, data)
+        res.send("User Updated Successfully");
+    } catch (err) {
+        res.status(404).send("Update Failed" + err.message);
     }
-
-    if (data?.skills?.length > 10) {
-      throw new Error(" : Skills cannot be more than 10");
-    }
-
-    await User.findByIdAndUpdate({ _id: userId }, data, {
-      runValidators: true, // this is for the enbaling the validation
-    }); // or we can write (userId, data)
-    res.send("User Updated Successfully");
-  } catch (err) {
-    res.status(404).send("Update Failed" + err.message);
-  }
 });
 
 //get profile
 
 app.get("/profile", async (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies);
-  res.send("Reading Cookie");
+    const cookies = req.cookies;
+    console.log(cookies);
+    res.send("Reading Cookie");
 });
 
 connectDb()
-  .then(() => {
-    console.log("Database Connection Estbalished");
-    app.listen(3000, () => {
-      console.log("Server is litening to the port no. 3000");
+    .then(() => {
+        console.log("Database Connection Estbalished");
+        app.listen(3000, () => {
+            console.log("Server is litening to the port no. 3000");
+        });
+    })
+    .catch((error) => {
+        console.error("Database Cannot be Connected.");
     });
-  })
-  .catch((error) => {
-    console.error("Database Cannot be Connected.");
-  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,88 +1,88 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 3,
+            maxLength: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            minLength: 3,
+            maxLength: 50,
+        },
+        emailId: {
+            type: String,
+            lowercase: true,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid email address : " + value);
+                }
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Enter a strong password : " + value);
+                }
+            },
+        },
+        age: {
+            type: Number,
+            min: 18,
+        },
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "female", "others"].includes(value)) {
+                    throw new Error("Gender data is not valid");
+                }
+            },
+        },
+        photoUrl: {
+            type: String,
+            default:
+                "https://media.istockphoto.com/id/1553217327/vector/user-profile-icon-avatar-person-sign-profile-picture-portrait-symbol-easily-editable-line.jpg?s=170667a&w=0&k=20&c=xUuHLFaa94WIFdV-XBgxX9SSsaJJgGQhE1Tmevqrytg=",
+            validate(value) {
+                if (!validator.isURL(value)) {
+                    throw new Error("Invalid Photo URL : " + value);
+                }
+            },
+        },
+        about: {
+            type: String,
+            default: "Default About of User",
+            maxLength: 50,
+        },
+        skills: {
+            type: [String],
+        },
     },
-    lastName: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    emailId: {
-      type: String,
-      lowercase: true,
-      required: true,
-      trim: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Invalid email address : " + value);
-        }
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-      validate(value) {
-        if (!validator.isStrongPassword(value)) {
-          throw new Error("Enter a strong password : " + value);
-        }
-      },
-    },
-    age: {
-      type: Number,
-      min: 18,
-    },
-    gender: {
-      type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
-      },
-    },
-    photoUrl: {
-      type: String,
-      default:
-        "https://media.istockphoto.com/id/1553217327/vector/user-profile-icon-avatar-person-sign-profile-picture-portrait-symbol-easily-editable-line.jpg?s=170667a&w=0&k=20&c=xUuHLFaa94WIFdV-XBgxX9SSsaJJgGQhE1Tmevqrytg=",
-      validate(value) {
-        if (!validator.isURL(value)) {
-          throw new Error("Invalid Photo URL : " + value);
-        }
-      },
-    },
-    about: {
-      type: String,
-      default: "Default About of User",
-      maxLength: 50,
-    },
-    skills: {
-      type: [String],
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 //email check
 userSchema.pre("save", async function (next) {
-  const existingUser = await mongoose.models.User.findOne({
-    emailId: this.emailId,
-  });
+    const existingUser = await mongoose.models.User.findOne({
+        emailId: this.emailId,
+    });
 
-  if (existingUser) {
-    const err = new Error("Email already exists");
-    err.status = 400;
-    return next(err);
-  }
+    if (existingUser) {
+        const err = new Error("Email already exists");
+        err.status = 400;
+        return next(err);
+    }
 
-  next();
+    next();
 });
 
 module.exports = mongoose.model("User", userSchema);
