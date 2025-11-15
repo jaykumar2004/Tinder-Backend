160ed63d1c732ed9a50d3837d8664d7faa57953d
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-11-15 16:05:52 +0530

    profile-edit, forgot- password api's

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,16 +1,57 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth.js");
+const { validateEditProfileData } = require("../utils/validation.js");
+const bcrypt = require("bcrypt");
 
 const profileRouter = express.Router();
 
 //get profile
-
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("Error : " + err.message);
+  }
+});
+
+//profile edit
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    await loggedInUser.save();
+    res.json({
+      message: `${loggedInUser.firstName}, Your Profile Updated Successfully`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+profileRouter.patch("/profile/change-password", userAuth, async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+
+  try {
+    const user = req.user;
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Current password is incorrect" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: "Password changed successfully." });
+  } catch (err) {
+    res.status(500).send("ERROR: " + err.message);
   }
 });
 
